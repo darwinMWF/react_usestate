caf11d95ea257bb7e588e32ccb8b8110ab0af19f
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-07-07 12:46:17 +0530

    add feature all ,complted,not completed and add option to edit

--- src/todo_list/Task.jsx
+++ src/todo_list/Task.jsx
@@ -1,41 +1,67 @@
- 
- import { useState } from "react"
-import List from "./list"
- const arry = [
-    {id:0,task:"reading",ischecked:false ,isedited:false},
-    {id:1,task:"eting",ischecked:false,isedited:false},
-    {id:2,task:"driving",ischecked:false,isedited:false},
-    {id:3,task:"learning",ischecked:false,isedited:false},
-    {id:4,task:"travelling",ischecked:false,isedited:false}
- ]
+import { useState } from "react";
+import List from "./list";
+const arry = [
+  { id: 0, task: "reading", ischecked: false, isedited: false },
+  { id: 1, task: "eting", ischecked: false, isedited: false },
+  { id: 2, task: "driving", ischecked: false, isedited: false },
+  { id: 3, task: "learning", ischecked: false, isedited: false },
+  { id: 4, task: "travelling", ischecked: false, isedited: false },
+];
 
+const Task = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [taskArry, setTaskArry] = useState(arry);
+  const [taskmajorArray, settaskchangeArray] = useState(arry);
+  const [newid, setnewid] = useState(5);
 
-const Task = ()=>{
-    const [inputValue,setInputValue] = useState('')
-    const [taskArry,setTaskArry] = useState(arry)
-    const [ newid,setnewid] =useState(5);
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+    setnewid(newid + 1);
+  };
 
-    const handleChange = (e)=>{
-        // console.log(e)
-        setInputValue(e.target.value);
-        setnewid(newid+1);
-    }
+  const handleAdd = () => {
+    setTaskArry([
+      ...taskArry,
+      { id: newid, task: inputValue, ischecked: false, isedited: false },
+    ]);
+    settaskchangeArray([
+      ...taskmajorArray,
+      { id: newid, task: inputValue, ischecked: false, isedited: false },
+    ]);
+    setInputValue("");
+  };
 
-    const handleAdd = ()=>{
-        setTaskArry([...taskArry,{id:newid,task:inputValue,ischecked:false,isedited:false}])
-        setInputValue('')
-    }
-    return <>
-    <input  value={inputValue} type="text" onChange={handleChange}/>
-    <button onClick={handleAdd}>Add</button>
-    <div>
-        <button>All</button>
-        <button>Completed</button>
-        <button>Not Completed</button>
-    </div>
-    <List array = {taskArry} setarry = {setTaskArry}/>
+  const alltaskhandler = () => {
+    setTaskArry(taskmajorArray);
+  };
 
+  const completehandler = () => {
+    const list = taskmajorArray.filter((x) => x.ischecked);
+    console.log(list);
+    setTaskArry(list);
+  };
+
+  const Notcompletehandler = () => {
+    const list = taskmajorArray.filter((x) => x.ischecked === false);
+    setTaskArry(list);
+  };
+  return (
+    <>
+      <input value={inputValue} type="text" onChange={handleChange} />
+      <button onClick={handleAdd}>Add</button>
+      <div>
+        <button onClick={alltaskhandler}>All</button>
+        <button onClick={completehandler}>Completed</button>
+        <button onClick={Notcompletehandler}>Not Completed</button>
+      </div>
+      <List
+        array={taskArry}
+        setarry={setTaskArry}
+        majorarry={taskmajorArray}
+        setmajorarry={settaskchangeArray}
+      />
     </>
-}
+  );
+};
 
-export default Task
+export default Task;
--- src/todo_list/list.jsx
+++ src/todo_list/list.jsx
@@ -1,32 +1,71 @@
 import { useState } from "react";
 import "./list.css";
 
-const List = ({ array, setarry }) => {
+const List = ({ array, setarry, majorarry, setmajorarry }) => {
 
-const [editinput,seteditInput] = useState('');
+  const [editInputValue, seteditInputvalue] = useState("");
 
   const deleteHandler = (id) => {
-    const newArrylist = array.filter((data) => data.id !== id);
+    const newArrylist = majorarry.filter((data) => data.id !== id);
     setarry(newArrylist);
+    setmajorarry(newArrylist);
+  };
+
+  const handleCheckbox = (id) => {
+    const newArry = majorarry.map((data) => {
+      if (data.id == id) {
+        return { ...data, ischecked: !data.ischecked ? true : false };
+      } else {
+        return data;
+      }
+    });
+    setmajorarry(newArry);
+    setarry(newArry);
   };
 
   const edithandler = (id) => {
     const list = array.map((obj) => {
       if (obj.id == id) {
-        return { ...obj, isedited:!obj.isedited };
+        if (obj.isedited) {
+          return { ...obj, task: editInputValue, isedited: false };
+        } else {
+          return { ...obj, isedited: true };
+        }
       } else {
         return obj;
       }
     });
 
     setarry(list);
+    setmajorarry(majorarry);
   };
 
-  console.log(array);
+  const editvaluehandler = (e) => {
+    seteditInputvalue(e.target.value);
+  };
+
+
   const listdata = array.map((x) => (
     <li key={x.id}>
-      <input type={x.isedited?"text":"checkbox"} defaultValue={x.isedited?x.task:''} />
-      {x.isedited?'':x.task}
+      {x.isedited && (
+        <input
+          type="text"
+          id={x.id}
+          onChange={editvaluehandler}
+          defaultValue={x.task}
+        />
+      )}
+      {!x.isedited && (
+        <input
+          type="checkbox"
+          id={x.id}
+          onChange={() => {
+            handleCheckbox(x.id);
+          }}
+          defaultChecked={x.ischecked ? true : false}
+        />
+      )}
+      {x.isedited ? "" : x.task}
       <button
         onClick={() => {
           edithandler(x.id);
